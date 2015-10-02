49fbd6e126604a11f5fcd0c55182c0658892de01
Mark St.Godard <markstgodard@gmail.com>
2015-10-02 14:54:12 -0500

    Error handling

--- serv.go
+++ serv.go
@@ -18,6 +18,13 @@ func main() {
 	}
 
 	dir := args[0]
+
+	fi, err := os.Stat(dir)
+	if err != nil || !fi.IsDir() {
+		fmt.Fprintf(os.Stderr, "Invalid directory: %s\n", dir)
+		os.Exit(1)
+	}
+
 	log.Printf("Serving from dir: %s\n", dir)
 
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(dir))))
